794942e0af5bffae359b1a0be934d24dd172cc58
analuisasanchezarauz <analuisasanchezarauz@ANALUISAS6E74>
2021-10-11 22:26:31 -0400

    Agregar archivos de proyecto.

---
+++ WebApplication1/Data/EstudioDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class EstudioDBContext: DbContext 
+    {
+         public EstudioDBContext (DbContextOptions<EstudioDBContext>options):base(options)
+        { 
+
+        }
+         public DbSet<WebApplication1.Models.Music> Songs { get; set; }
+        public object Music { get; internal set; }
+    }
+}
---
+++ WebApplication1/Models/Music.cs
@@ -0,0 +1,29 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Music
+    {
+   
+        [Key]
+        public int MusicId { get; set; }
+        [Required(ErrorMessage = "El nombre de la cancion es requerido")]
+        [StringLength (60, MinimumLength =3, ErrorMessage="El nombre de la cancion debe tener mas de 3 letras")]
+        [Display(Name = "Nombre completo de la cancion")]
+        public string Nombre { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre del autor es requerido")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "El nombre del autor debe tener mas de 3 letras")]
+        [Display(Name = "Nombre del autor")]
+        public string Autor { get; set; }
+
+        [Required(ErrorMessage = "El nombre del autor es requerido")]
+        [Display(Name = "Letra")]
+        public string Letra { get; set; }
+
+
+
+    }
+}
